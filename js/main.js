ed909dc42b1b39a320a61af42a34639b54c5635b
Yulia2120 <ket-dz@rambler.ru>
2022-08-28 07:31:41 +0300

    first commit

---
+++ js/main.js
@@ -0,0 +1,10 @@
+// $(function () {
+//   $(".open-search").click(function (e) {
+//     e.preventDefault();
+//     $("#search").addClass("active");
+//   });
+
+//   $(".close-search").click(function () {
+//     $("#search").removeClass("active");
+//   });
+// });
